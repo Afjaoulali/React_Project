9ef6b03d874725c7bc58efa8241af06af781c98e
Afjaoulalie <jaoulaliafaf@gmail.com>
2022-05-02 23:44:41 +0000

    exo+revision

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ console.log("application react");
 if (true) {
   console.log("cgghjjg");
 }
+import {
+  bonjour,
+  bonjour2,
+  displayNotes,
+  displayNotesClassique,
+  displayStudentName,
+  student,
+  calculateAverageClassique,
+  calculateAverage,
+  displayStudent,
+} from "./exos/revision";
+
+console.log(bonjour("John"));
+console.log(bonjour2("john"));
+console.log(displayStudentName(student));
+console.log(displayNotesClassique(student.notes));
+console.log(calculateAverage(student.notes));
+console.log(calculateAverageClassique(student.notes));
+console.log(displayStudent(student));
