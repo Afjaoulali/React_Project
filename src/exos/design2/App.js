919c4ecde578c3bca0b5ba7db1d788675fa207c3
Afjaoulalie <jaoulaliafaf@gmail.com>
2022-05-03 14:40:39 +0000

    Premier Design

--- src/exos/design2/App.js
+++ src/exos/design2/App.js
@@ -1,9 +1,3 @@
 import React from "react";
 
-/**
- * Contient tout les composants (ainsi
- * que le css pour le design 1)
- *
- * @ref https://www.figma.com/file/uqUjILdONGoXCEImw1iYqv/Untitled?node-id=0%3A1
- */
 export default function App() {}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-/**
- * Créer un composant Hello qui affiche
- * une h1 avec "Hello {name} !"
- *
- * Ce composant doit être dans son propre fichier
- * et exporté par défaut
- *
- * Importé le composant dans index.js et l'afficher
- * dans le render
- */
 import React from "react";
 import { render } from "react-dom";
+import App from "./exos/design1/App";
 import "./index.css";
-import App from "./App";
+
 render(<App />, document.querySelector("#root"));
