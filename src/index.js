e581113a1158b25752630a5c7d346083bb7df7f9
Afjaoulalie <jaoulaliafaf@gmail.com>
2022-05-03 10:15:48 +0000

    Ajout du composant App

--- src/index.js
+++ src/index.js
@@ -10,5 +10,5 @@
  */
 import React from "react";
 import { render } from "react-dom";
-import Hello from "./Hello";
-render(<Hello />, document.querySelector("#root"));
+import App from "./App";
+render(<App />, document.querySelector("#root"));
