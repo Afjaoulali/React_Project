33c5fdf5053f063d0382724690b9b1bfdf2f8e9d
Afjaoulalie <jaoulaliafaf@gmail.com>
2022-05-04 08:08:25 +0000

    Ajout d'un exemple avec styled component

---
+++ src/exos/design-styled-component/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { HeaderBar } from "./styled";
+
+export default function App() {
+  return (
+    <>
+      <HeaderBar>
+        <Logo />
+        <Menu />
+      </HeaderBar>
+      <Content>
+        <Card></Card>
+        <Card></Card>
+        <Card>
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardNotif />
+        </Card>
+      </Content>
+      <FooterBar>
+        <Logo />
+        <FooterMenu />
+      </FooterBar>
+    </>
+  );
+}
