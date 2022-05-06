61c6a7ae1ed8a14d333b6cf058fdfd8bbd41d8be
Afjaoulalie <jaoulaliafaf@gmail.com>
2022-05-06 08:11:45 +0000

    Ajout de state pour les champs du formulaire

--- src/todo-app/pages/CreateAccount.js
+++ src/todo-app/pages/CreateAccount.js
@@ -2,32 +2,47 @@ import React, { useState } from "react";
 
 export default function CreateAccount() {
   const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+
+  const changeEmail = (ev) => setEmail(ev.target.value);
+  const changePassword = (ev) => setPassword(ev.target.value);
+  const changeRepeatedPassword = (ev) => setRepeatedPassword(ev.target.value);
+
   const sendAccount = (ev) => {
     // Pour annuler le rechargement de page,
     // nous utilisons "preventDefault"
     ev.preventDefault();
     setLoading(true);
 
-    //pour récupperer les donné de mon form: 2ème facon
-    const email = ev.target.email.value;
-    const password = ev.target.password.value;
-    console.log(email, password);
+    console.log(email, password, repeatedPassword);
   };
 
   return (
     <>
       <h1>Inscription</h1>
       <form onSubmit={sendAccount}>
-        <input type="email" name="email" placeholder="Votre email ..." />
         <input
-          type="password"
-          name="password"
-          placeholder="Votre mot de passe ..."
+          type="email"
+          name="email"
+          placeholder="Votre email ..."
+          value={email}
+          onChange={changeEmail}
         />
         <input
           type="password"
           name="password"
+          placeholder="Votre mot de passe ..."
+          value={password}
+          onChange={changePassword}
+        />
+        <input
+          type="password"
+          name="repeatedPassword"
           placeholder="Répéter votre mot de passe ..."
+          value={repeatedPassword}
+          onChange={changeRepeatedPassword}
         />
         {loading ? (
           <p>Chargement ...</p>
